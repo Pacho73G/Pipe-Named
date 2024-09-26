e64277cbf5c1428fcf968fb8bd3eb3a0253e1365
Francisco Guzman <144657190+Pacho73G@users.noreply.github.com>
2024-09-25 22:51:14 -0500

    Update servidor.c

--- servidor.c
+++ servidor.c
@@ -16,21 +16,24 @@ Topico: Comunicacion uni-Direccional
 
 #define FIFO_FILE "MYFIFO"
   int main() {
+    // Creación de variables
      int ArchivoDesc;
      char BusMensaje[80];
      char final[10];
      int fin;
      int cantidadBytes;
      printf("\n \t\t >>>>>> INICIO SERVIDOR <<<<<<  \n");
-     /* Create the FIFO if it does not exist */
+     // Crear el FIFO si no existe, con permisos de lectura/escritura para el propietario
      mknod(FIFO_FILE, S_IFIFO|0640, 0);
-     strcpy(final, "end");
+     strcpy(final, "end");// Copia "end" en la variable final para la comparación
      while(1) {
        ArchivoDesc = open(FIFO_FILE, O_RDONLY);
        cantidadBytes = read(ArchivoDesc, BusMensaje, sizeof(BusMensaje));
        BusMensaje[cantidadBytes] = '\0';
+
+       // Muestra el mensaje recibido y la cantidad de bytes
        printf("Cadena recibida: \"%s\" y cantidad de bytes %d\n", BusMensaje, (int)strlen(BusMensaje));
-        fin = strcmp(BusMensaje, final);
+        fin = strcmp(BusMensaje, final); // Compara el mensaje recibido con "fin"
         if (fin == 0) {
            close(ArchivoDesc);
            break;
